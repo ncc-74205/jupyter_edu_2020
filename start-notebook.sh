c46b397a610c96e9ae78605ceffc0960e352e39b
ncc-74205 <40716826+ncc-74205@users.noreply.github.com>
2021-04-03 13:30:23 +0200

    Update start-notebook.sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,11 +1,11 @@
-#mysqld --host=127.0.0.1 --port=3306 --skip-grant-tables &
-/usr/local/bin/start.sh mysqld --init-file=/home/jovyan/mysql-init &
+#deactivated in order to have to start servers manually like in mybinder
 
-export PGDATA=/home/jovyan/pgdata
-mkdir $PGDATA
-pg_ctl initdb
-pg_ctl start
-psql -c "create database DEMO;" postgres
+#/usr/local/bin/start.sh mysqld --init-file=/home/jovyan/mysql-init &
+#export PGDATA=/home/jovyan/pgdata
+#mkdir $PGDATA
+#pg_ctl initdb
+#pg_ctl start
+#psql -c "create database DEMO;" postgres
 
 /usr/local/bin/start.sh jupyter lab --NotebookApp.token='' $*
 #/usr/local/bin/start.sh jupyter notebook --NotebookApp.token='' $*
